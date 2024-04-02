222efa4cf52fd010b1992be9e11842c801641eab
crathod584 <crathod584@gmail.com>
2024-04-02 16:11:53 +0530

    Update userAction.ts

--- src/store/userAction.ts
+++ src/store/userAction.ts
@@ -1,12 +1,12 @@
 // userActions.ts
 import axios from 'axios';
 import { Dispatch } from 'redux';
-import { ADD_USER, DELETE_USER, FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, UPDATE_USER_LOCATION, User } from '../utils/types';
+import { ADD_USER, DELETE_USER, FETCH_USERS_FAILURE, START_LOADING, FETCH_USERS_SUCCESS, UPDATE_USER_LOCATION, User } from '../utils/types';
 
 
 export const fetchUsers = () => {
     return async (dispatch: Dispatch) => {
-      dispatch({ type: FETCH_USERS_REQUEST });
+      dispatch({ type: START_LOADING });
       try {
         const response = await axios.get<User[]>('https://660160fd87c91a11641ab523.mockapi.io/users');
         dispatch({ type: FETCH_USERS_SUCCESS, payload: response.data });
@@ -22,6 +22,7 @@ export const addUser = (newUser: User) => {
 
 export const deleteUser = (id: string) => {
     return async (dispatch: Dispatch) => {
+        dispatch({ type: START_LOADING });
         try {
             await axios.delete(`https://660160fd87c91a11641ab523.mockapi.io/user/${id}`);
             dispatch({ type: DELETE_USER, payload: id });
@@ -33,6 +34,7 @@ export const deleteUser = (id: string) => {
 
 export const updateUserLocation = (updatedUser: User) => {
     return async (dispatch: Dispatch) => {
+        dispatch({ type: START_LOADING });
         try {
             const response = await axios.put(`https://660160fd87c91a11641ab523.mockapi.io/users/${updatedUser.id}`, updatedUser);
             dispatch({ type: UPDATE_USER_LOCATION, payload: updatedUser });
